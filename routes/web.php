b1b054961ffb235e8fee9065e6fd2dbe09b1c170
ZIZOU-LA-VARIATION <keucheduvalier339@gmail.com>
2025-03-21 13:50:53 +0100

    add-auth

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// user dashboar route
+Route::get('user', function () {
+    return view('user.dashboard');
+})->name('dashboard');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('user', function () {
+    return view('user.dashboard');
+})->name('user-profile');
